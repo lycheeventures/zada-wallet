b5e846c3c0e987f9f17e1f9160d3c4d0148c789d
samin-labz <saddad@auroralabz.com>
2022-06-01 11:12:42 +0500

    ZM-294: Fixed date time issues.

--- covidWallet/app/helpers/time.js
+++ covidWallet/app/helpers/time.js
@@ -1,23 +1,41 @@
 import moment from 'moment';
 
+var dateFormats = [
+  // with forward slash
+  "YYYY/MM/DD",
+  "MM/DD/YYYY",
+  "DD/MM/YYYY",
+  // with hyphen
+  "YYYY-MM-DD",
+  "MM-DD-YYYY",
+  "DD-MM-YYYY",
+];
+
+var timeFormats = [
+  "YYYY-MM-DD[T]HH:mm:ss.SSSZ",
+  "MM-DD-YYYY[T]HH:mm:ss.SSSZ",
+  "DD-MM-YYYY[T]HH:mm:ss.SSSZ",
+  // with forward slash
+  "YYYY/MM/DD HH:mm:ss",
+  "MM/DD/YYYY HH:mm:ss",
+  "DD/MM/YYYY HH:mm:ss",
+  // with hyphen
+  "YYYY-MM-DD HH:mm:ss",
+  "MM-DD-YYYY HH:mm:ss",
+  "DD-MM-YYYY HH:mm:ss",
+];
+
+
 export const get_local_issue_time = (issueTime) => {
-  if (moment.utc(issueTime, 'mm/dd/yyyy hh:mm:ss').isValid()) {
-    return moment
-      .utc(issueTime, 'MM/DD/YYYY hh:mm:ss')
-      .local()
-      .format('DD/MM/YYYY HH:mm')
-      .toString();
-  } else {
-    let formattedDate = moment(issueTime).format('DD/MM/YYYY HH:mm');
-    if (formattedDate !== 'Invalid date') {
-      return formattedDate;
-    }
-    return issueTime;
-  }
+  return moment
+    .utc(issueTime, timeFormats)
+    .local()
+    .format('DD/MM/YYYY HH:mm')
+    .toString();
 };
 
 export const get_local_issue_date = (issueTime) => {
-  if (moment.utc(issueTime, 'mm/dd/yyyy hh:mm:ss').isValid()) {
+  if (moment.utc(issueTime, dateFormats).isValid()) {
     return moment
       .utc(issueTime, 'MM/DD/YYYY hh:mm:ss')
       .local()
@@ -32,14 +50,31 @@ export const get_local_issue_date = (issueTime) => {
   }
 };
 
-export const changeDateFormat = (date) => {
-  return moment(date).format('DD/MM/YYYY');
-};
-
-export const formatDateWithHyphen = (date) => {
-  return moment(date).format('DD-MM-YYYY');
-};
-
 export const get_local_date_time = (date) => {
   return moment(date).format('DD/MM/YYYY hh:mm A');
 };
+
+export const is_date_time = (val) => {
+  if (!moment(val, timeFormats, true).isValid()) return false
+
+  return true
+}
+
+export const is_date = (val) => {
+  if (!moment(val, dateFormats, true).isValid()) return false
+
+  return true
+}
+
+export const parse_date_time = (val) => {
+  if (is_date(val)) {
+    return moment(val).format('DD/MM/YYYY');
+  }
+
+  if (is_date_time(val)) {
+    console.log('its date _time')
+    return get_local_issue_time(val);
+  }
+
+  return val
+}
