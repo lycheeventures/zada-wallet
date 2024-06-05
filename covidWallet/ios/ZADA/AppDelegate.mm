70697490dbbfdb12bee4d4ed762044fa232fc441
Saddad Amin <saddad@nordivo.com>
2024-06-05 18:19:09 +0500

    version updated to 0.73.8

--- covidWallet/ios/ZADA/AppDelegate.mm
+++ covidWallet/ios/ZADA/AppDelegate.mm
@@ -32,6 +32,12 @@
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self getBundleURL];
+}
+ 
+- (NSURL *)getBundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
